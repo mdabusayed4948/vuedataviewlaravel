cbda99a3900c887fcd5aa9aa817f68b29a2e7887
sayedme120 <sayedme120@gmail.com>
2020-10-06 06:03:02 +0600

    Create Api CustomerController, CustomerCollection, CustomerResource and Create Customer Model, Factory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                    <a href="{{ route('customers') }}">Customers</a>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -81,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelVueDataViewer
                 </div>
 
                 <div class="links">
